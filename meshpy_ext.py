9845064483272bc0732e489e915662e93225c201
dlilien <skinonframe@gmail.com>
2015-04-30 15:11:19 -0700

    cleaned with autopep

--- meshpy_ext.py
+++ meshpy_ext.py
@@ -13,10 +13,12 @@ Subclass the meshpy class to add conveniences
 from meshpy import gmsh_reader
 import numpy as np
 
+
 class gmshNumpy(gmsh_reader.GmshMeshReceiverNumPy):
-    def __init__(self,fn):
-        super(gmshNumpy,self).__init__()
-        gmsh_reader.read_gmsh(self,fn)
+
+    def __init__(self, fn):
+        super(gmshNumpy, self).__init__()
+        gmsh_reader.read_gmsh(self, fn)
+
     def CreateBases(self):
         for element in self.elements:
-            
